90b7bef1a11ede459af3ac532dc95479ce574935
Sasha <sashafr@sas.upenn.edu>
2014-05-29 12:29:30 -0400

    updates to home.html and utils.py
    added seo to home page and added utils.py for common helper functions
    that are not exactly middleware

--- base/models.py
+++ base/models.py
@@ -1,1 +1,9 @@
 """ Basic models, such as user profile """
+from django.db import models
+
+class GlobalVars(models.Model):
+    variable = models.CharField(max_length = 200)
+    val = models.TextField()
+	
+    def __unicode__(self):
+        return self.val  
--- base/views.py
+++ base/views.py
@@ -1,8 +1,13 @@
 """ Views for the base application """
 
 from django.shortcuts import render
-
+from base.models import GlobalVars
+from base.utils import load_globals
 
 def home(request):
     """ Default view for the root """
-    return render(request, 'base/home.html')
+	
+    """ Load global variables at add to context """
+    context = load_globals()
+	
+    return render(request, 'base/home.html', context)
