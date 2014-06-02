8d560a2d6cd7f0ade62f6fd7237ab535f5560ef6
Sasha <sashafr@sas.upenn.edu>
2014-06-02 16:57:41 -0400

    featured images on home page
    Began html and models for displaying featured images on homepage

---
+++ base/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from base.models import GlobalVars, Media, MediaType
+
+admin.site.register(GlobalVars)
+admin.site.register(Media)
+admin.site.register(MediaType)
--- base/models.py
+++ base/models.py
@@ -1,9 +1,29 @@
 """ Basic models, such as user profile """
 from django.db import models
+from django.contrib.auth.models import User
 
 class GlobalVars(models.Model):
     variable = models.CharField(max_length = 200)
     val = models.TextField()
 	
     def __unicode__(self):
-        return self.val  
+        return self.variable
+
+class MediaType(models.Model):
+    type = models.CharField(max_length = 40)
+
+    def __unicode__(self):
+        return self.type    
+        
+class Media(models.Model):
+    title = models.CharField(max_length = 100)
+    type = models.ForeignKey(MediaType)
+    featured = models.BooleanField(default = False)
+    uri = models.URLField(blank = True)
+    notes = models.TextField(blank = True)
+    created = models.DateTimeField(auto_now = False, auto_now_add = True)
+    modified = models.DateTimeField(auto_now = True, auto_now_add = False)
+    last_mod_by = models.ForeignKey(User)
+    
+    def __unicode__(self):
+        return self.title
